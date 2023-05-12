c48f84dc4a0e83171f0cbe8cafae298c6ee8ae76
MarcoF20 <piratafranco20@gmail.com>
2023-05-11 20:34:27 -0700

    Added read and create database functions

--- app/src/main/java/com/example/p6basededatos/DatabaseHelper.java
+++ app/src/main/java/com/example/p6basededatos/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.p6basededatos;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -23,10 +25,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_CONTACT_TABLE = "CREATE TABLE " + DATABASE_TABLE+"("+KEY_ID+"INTEGER PRIMARY KEY,"+KEY_NAME+"TEXT,"+KEY_PhoneNumber+"TEXT)";
+        db.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS "+ DATABASE_TABLE);
+    }
 
+    //CRUD OPERATIONS
+    //Agregar contacto
+
+    public void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PhoneNumber, contact.getPhoneNumber());
+        db.insert(DATABASE_TABLE,null, values);
+        db.close();
+    }
+    public Contact readContact(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(DATABASE_TABLE, new String[]{KEY_ID,KEY_NAME,KEY_PhoneNumber}, KEY_ID+"=?",
+                new String[]{String.valueOf(id)}, null,null,null,null);
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1),
+                cursor.getString(2));
+        return contact;
     }
 }
